26630ce56f57b9cf53024e04684cab733ef9c4bb
Emad <67466634+itsEmadK@users.noreply.github.com>
2025-01-23 22:43:08 +0330

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+    entry: "./src/index.js",
+    mode: "development",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+
+}

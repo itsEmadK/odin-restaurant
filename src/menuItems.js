db18e2c26aa874e0759697e0b27ed7b051d3a322
Emad <67466634+itsEmadK@users.noreply.github.com>
2025-01-24 14:09:46 +0330

    Add menu item informatio

---
+++ src/menuItems.js
@@ -0,0 +1,53 @@
+import carneImage from "./images/pizzas/carne.png";
+import coloratoImage from "./images/pizzas/colorato.png";
+import cremaImage from "./images/pizzas/crema.png";
+import disgustosoImage from "./images/pizzas/disgustoso.png";
+import gamberiImage from "./images/pizzas/gamberi.png";
+import pepeImage from "./images/pizzas/pepe.png";
+import pomodoroImage from "./images/pizzas/pomodoro.png";
+import salsicciaImage from "./images/pizzas/salsiccia.png";
+
+const menuItems = [
+    {
+        img: salsicciaImage,
+        title: "Salsiccia",
+        desc: "Tomato sauce, Mozarella, Tomato, Homemade sausage, Garlic, Basil"
+    },
+    {
+        img: gamberiImage,
+        title: "Gamberi",
+        desc: "Tomato sauce, Mozarella, Shrimps, Feta cheese, Olives, Basil"
+    },
+    {
+        img: pepeImage,
+        title: "Pepe",
+        desc: "Tomato sauce, Mozarella, Chilli flakes, Olives, Basil"
+    },
+    {
+        img: disgustosoImage,
+        title: "Disgustoso",
+        desc: "Tomato sauce, Bacon, Pineapple, Olives, Basil"
+    },
+    {
+        img: coloratoImage,
+        title: "Colorato",
+        desc: "Tomato sauce, Mozarella, Onion, Pepper, Champignons, Basil"
+    },
+    {
+        img: pomodoroImage,
+        title: "Pomodoro",
+        desc: "Tomato sauce, Mozarella, Tomato, Onion, Feta cheese, Chilli"
+    },
+    {
+        img: cremaImage,
+        title: "Crema",
+        desc: "White sauce, Mozarella, Shrimps, Salmon, Pineapple, Olives, Basil"
+    },
+    {
+        img: carneImage,
+        title: "Carne",
+        desc: "Tomato sauce, Mozarella, Homemade sausage, Bacon, Garlic, Pepper, Chilli"
+    },
+]
+
+export { menuItems };

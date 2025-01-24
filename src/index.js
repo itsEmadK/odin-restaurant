f3eabb2963f7140fe5e8a3a228af153cd9f780da
Emad <67466634+itsEmadK@users.noreply.github.com>
2025-01-24 16:33:51 +0330

    Add tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import { loadHomepage } from "./loadHomepage.js";
+import { loadMenuPage } from "./loadMenuPage.js";
+import { loadContactPage } from "./loadContactPage.js";
+import "./styles.css";
+
+
+const contentDiv = document.querySelector("div#content");
+const homeButton = document.querySelector("button.home");
+const menuButton = document.querySelector("button.menu");
+const contactButton = document.querySelector("button.contact");
 
 loadHomepage();
+homeButton.classList.add("selected");
+
+
+homeButton.addEventListener("click", () => {
+    contentDiv.innerHTML = "";
+    loadHomepage();
+    homeButton.classList.add("selected");
+    menuButton.classList.remove("selected");
+    contactButton.classList.remove("selected");
+});
+
+
+menuButton.addEventListener("click", () => {
+    contentDiv.innerHTML = "";
+    loadMenuPage();
+    homeButton.classList.remove("selected");
+    menuButton.classList.add("selected");
+    contactButton.classList.remove("selected");
+});
+
+
+contactButton.addEventListener("click", () => {
+    contentDiv.innerHTML = "";
+    loadContactPage();
+    homeButton.classList.remove("selected");
+    menuButton.classList.remove("selected");
+    contactButton.classList.add("selected");
+});
+
+
